17ed15784b230c7948d5ebcfeda5c51d38b9304b
GustavoSilverio <80285864+GustavoSilverio@users.noreply.github.com>
2022-05-30 09:08:16 -0300

    Desafio  de javascript
    Descobrindo sua idade e seu gênero e mostrando na tela, com uma imagem baseada em ambos

---
+++ desafio 30_05_22/script.js
@@ -0,0 +1,11 @@
+document.getElementById("button").onclick = function () {
+    var radios = document.getElementsByName("sex")
+    var ano = (document.getElementById('ano')).value
+    for (var i = 0; i < radios.length; i++) {
+        if (radios[i].checked) {
+            var escolha = radios[i].value
+            console.log("Escolheu: " + escolha);
+            resultado(escolha, ano)
+        }
+    }
+};
